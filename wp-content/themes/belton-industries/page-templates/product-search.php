fd9849eccfd53332b646e86458546cc2a3c3a6b1
Joel <joel@Joels-MacBook-Air-2.local>
2017-09-22 07:49:37 -0400

    dev

---
+++ wp-content/themes/belton-industries/page-templates/product-search.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Product Search
+*/
+get_header(); ?>
+
+<section class="short-header">
+	<h1 class="entry-header">Product Search</h1>
+</section>
+
+
+
+<?php get_footer();
