9509ab7d2af5cc519e44ae4bece3ea40eebe7ed8
Joel <joel@Joels-MacBook-Air-2.local>
2017-09-25 13:37:16 -0400

    dev

--- wp-content/themes/belton-industries/page-templates/product-guide.php
+++ wp-content/themes/belton-industries/page-templates/product-guide.php
@@ -5,8 +5,13 @@ Template Name: Product Guide
 get_header(); ?>
 
 <section class="short-header text-center with-breadcrumb">
-	<h1 class="entry-header">Product Guide</h1>
-</section>
+	<div class="row">
+		<div class="large-12 columns">
+			<p class="breadcrumb back"><?php get_template_part('assets/images/right', 'arrow.svg'); ?> Back to Product Search</p>
+			<h1 class="entry-header">Product Guide</h1>
+		</div> <!-- columns -->
+	</div> <!-- row -->
+</section> <!-- short-header -->
 
 <div class="row">
 	<?php get_template_part('template-parts/product','guide'); ?>
