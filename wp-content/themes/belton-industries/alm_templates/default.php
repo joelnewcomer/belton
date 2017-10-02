db4af9c1fd945955fa6d61c0a57992bfe9907297
Joel <joel@Joels-MacBook-Air-2.local>
2017-10-02 09:21:47 -0400

    dev

---
+++ wp-content/themes/belton-industries/alm_templates/default.php
@@ -0,0 +1,16 @@
+<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry entry-content sr'); ?>>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="featured-image">
+			<?php the_post_thumbnail( array( 'width' => 268, 'height' => 164, 'crop' => true )); ?>
+		</div>
+	<?php endif; ?>
+	<div class="content-container">
+		<header>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php drum_entry_meta(); ?><br />
+		</header>
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div>
+	</div>
+</div>
--- wp-content/themes/belton-industries/index.php
+++ wp-content/themes/belton-industries/index.php
@@ -41,25 +41,8 @@ get_header(); ?>
 			
 			<div class="large-12 columns">
 				<article>
-				<?php if ( have_posts() ) : ?>
 				
-				    <?php /* Start the Loop */ ?>
-				    <?php while ( have_posts() ) : the_post(); ?>
-				        <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-				    <?php endwhile; ?>
-				
-				    <?php else : ?>
-				        <?php get_template_part( 'template-parts/content', 'none' ); ?>
-				
-				    <?php endif; // End have_posts() check. ?>
-				
-				    <?php /* Display navigation to next/previous pages when applicable */ ?>
-				    <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-				        <nav id="post-nav">
-				            <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				            <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-				        </nav>
-				    <?php } ?>
+				    <?php echo do_shortcode('[ajax_load_more post_type="post" scroll="false" repeater="default" posts_per_page="10" transition="fade" button_label="Load More"]'); ?>
 				
 				</article>
 			</div> <!-- columns -->
