967d35976fc2f6983cd0114b5fc40c3c1a06f522
Joel <joel@Joels-MacBook-Air-2.local>
2017-10-02 08:35:46 -0400

    dev

--- wp-content/themes/belton-industries/index.php
+++ wp-content/themes/belton-industries/index.php
@@ -35,31 +35,34 @@ get_header(); ?>
 					    <input type="text" placeholder="Search and hit Enter" name="s" id="s" />
 					    <!-- <input type="submit" id="searchsubmit" class="button fa" value="&#61442;" /> -->
 					</form>
-
-					<article>
-					<?php if ( have_posts() ) : ?>
-					
-					    <?php /* Start the Loop */ ?>
-					    <?php while ( have_posts() ) : the_post(); ?>
-					        <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-					    <?php endwhile; ?>
-					
-					    <?php else : ?>
-					        <?php get_template_part( 'template-parts/content', 'none' ); ?>
-					
-					    <?php endif; // End have_posts() check. ?>
-					
-					    <?php /* Display navigation to next/previous pages when applicable */ ?>
-					    <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-					        <nav id="post-nav">
-					            <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-					            <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-					        </nav>
-					    <?php } ?>
-					
-					</article>
 				</div> <!-- columns -->
 			</div> <!-- shadow-container-inner -->
+			<div class="shadow-fade"></div>
+			
+			<div class="large-12 columns">
+				<article>
+				<?php if ( have_posts() ) : ?>
+				
+				    <?php /* Start the Loop */ ?>
+				    <?php while ( have_posts() ) : the_post(); ?>
+				        <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+				    <?php endwhile; ?>
+				
+				    <?php else : ?>
+				        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+				
+				    <?php endif; // End have_posts() check. ?>
+				
+				    <?php /* Display navigation to next/previous pages when applicable */ ?>
+				    <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+				        <nav id="post-nav">
+				            <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+				            <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+				        </nav>
+				    <?php } ?>
+				
+				</article>
+			</div> <!-- columns -->
 		</div> <!-- shadow-container -->
 	</div> <!-- row -->
 </section>
--- wp-content/themes/belton-industries/template-parts/content.php
+++ wp-content/themes/belton-industries/template-parts/content.php
@@ -11,19 +11,18 @@
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry entry-content sr'); ?>>
-	<header>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="featured-image">
-				<?php the_post_thumbnail( array( 'width' => 268, 'height' => 164, 'crop' => true )); ?>
-			</div>
-		<?php endif; ?>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php drum_entry_meta(); ?><br />
-	</header>
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="featured-image">
+			<?php the_post_thumbnail( array( 'width' => 268, 'height' => 164, 'crop' => true )); ?>
+		</div>
+	<?php endif; ?>
+	<div class="content-container">
+		<header>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php drum_entry_meta(); ?><br />
+		</header>
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div>
 	</div>
-	<footer>
-		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
 </div>
