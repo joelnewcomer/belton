83ce1aaba9274d83a13d4d6e73493506e8e3167c
Joel <joel@Joels-MacBook-Air-2.local>
2017-12-11 11:08:11 -0500

    tweak

--- wp-content/themes/belton-industries/single-products.php
+++ wp-content/themes/belton-industries/single-products.php
@@ -68,13 +68,10 @@ get_header(); ?>
 			?>
 			</div> <!-- tag-filters -->
 		</section>		
-		<?php $installation = get_field('installation_guideline'); ?>
+		<?php $data_sheet = get_field('product_data_sheet'); ?>
 		<section class="downloads text-center">
 			<?php if ($data_sheet != ""): ?>
-				<div class="button icon arrow blue"><a href="<?php echo $installation; ?>" target="_blank"><?php get_template_part('assets/images/doc', 'icon.svg'); ?>Download Product Data Sheet<?php get_template_part('assets/images/right', 'arrow.svg'); ?></a></div>
-			<?php endif; ?>
-			<?php if ($installation != ""): ?>
-				<div class="button icon arrow blue"><a href="<?php echo $installation; ?>" target="_blank"><?php get_template_part('assets/images/doc', 'icon.svg'); ?>Download Installation Guideline<?php get_template_part('assets/images/right', 'arrow.svg'); ?></a></div>
+				<div class="button icon arrow blue"><a href="<?php echo $data_sheet; ?>" target="_blank"><?php get_template_part('assets/images/doc', 'icon.svg'); ?>Download Product Data Sheet<?php get_template_part('assets/images/right', 'arrow.svg'); ?></a></div>
 			<?php endif; ?>
 		</section>
 		<?php get_template_part('template-parts/not','finding'); ?>
