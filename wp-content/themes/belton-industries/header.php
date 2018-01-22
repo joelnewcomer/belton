5a628bfe95b9942d9afcbafa750c523b5094269a
Joel <joel@Joels-MacBook-Air-2.local>
2018-01-22 14:43:54 -0500

    Tweak

--- wp-content/themes/belton-industries/header.php
+++ wp-content/themes/belton-industries/header.php
@@ -26,8 +26,16 @@
 		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/touch-icon-ipad-retina.png">
 
 
-		<script src="https://use.typekit.net/kst4dbu.js"></script>
-		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+		<script>
+		  (function(d) {
+		    var config = {
+		      kitId: 'kst4dbu',
+		      scriptTimeout: 3000,
+		      async: true
+		    },
+		    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+		  })(document);
+		</script>
 
 		<?php wp_head(); ?>
 
