11067f4c13aed12d5ea2242c09a59351892f0f7b
Joel Newcomer <joel@drumcreative.com>
2019-04-23 09:15:46 -0400

    Updates

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.1' );
-define( 'REDIRECTION_BUILD', '3fbada17a3d9bd9b1737f7bf4721151f' );
+define( 'REDIRECTION_VERSION', '4.2.3' );
+define( 'REDIRECTION_BUILD', '832f7155834bcdc64325009360c6273b' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
